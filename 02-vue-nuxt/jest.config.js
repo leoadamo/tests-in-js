ae1265ad3f4d1ddae587f89b386780c6c9820c35
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2023-02-18 13:15:37 -0300

    feat: Renaming jest preset file

--- 02-vue-nuxt/jest.config.js
+++ 02-vue-nuxt/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	preset: './jest-preset.js',
+	preset: './jest.preset.js',
 	testEnvironment: 'jsdom',
 	testEnvironmentOptions: {
 		customExportConditions: ['node', 'node-addons'],
