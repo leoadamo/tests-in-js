70f83d570f23f7de79c012faa1f4323dd1d29688
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2023-02-20 23:44:36 -0300

    test: Husky hooks

--- 02-vue-nuxt/test/NuxtLogo.spec.js
+++ 02-vue-nuxt/test/NuxtLogo.spec.js
@@ -2,6 +2,8 @@ import { mount } from '@vue/test-utils';
 import NuxtLogo from '@/components/NuxtLogo.vue';
 
 describe('NuxtLogo', () => {
+  const name = 'test';
+
   test('is a Vue instance', () => {
     const wrapper = mount(NuxtLogo);
     expect(wrapper.vm).toBeTruthy();
