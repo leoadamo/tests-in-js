114f21af97ab4743efa40994a4d26cf8c952857e
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2023-02-22 18:28:28 -0300

    test(02-vue-nuxt): adding ProductCard tests

---
+++ 02-vue-nuxt/components/ProductCard.unit.spec.js
@@ -0,0 +1,69 @@
+// DEPENDENCIES
+import { mount } from '@vue/test-utils';
+import { makeServer } from '@/miragejs/server';
+
+// COMPONENTS
+import ProductCard from '@/components/ProductCard';
+
+/**
+ * Creates a fresh instance of a ProductCard component.
+ *
+ * @param {object} server The miragejs server instance.
+ *
+ * @returns {object} The ProductCard instance.
+ */
+function mountProductCard(server, options = {}) {
+	const product = server.create('product', {
+		title: 'Dummy test',
+		price: '23.00',
+		image: 'https://loremflickr.com/640/480/food?lock=96682',
+	});
+
+	const wrapper = mount(ProductCard, {
+		...options,
+		propsData: {
+			product,
+		},
+	});
+
+	return {
+		wrapper,
+		product,
+	};
+}
+
+describe('ProductCard - Unit', () => {
+	let server;
+
+	beforeEach(() => {
+		server = makeServer();
+	});
+
+	afterEach(() => {
+		server.shutdown();
+	});
+
+	it('Should mount the component', () => {
+		const { wrapper } = mountProductCard(server);
+
+		expect(wrapper.vm).toBeTruthy();
+		expect(wrapper.text()).toContain('Dummy test');
+		expect(wrapper.text()).toContain('23.00');
+	});
+
+	it('Should match snapshot', () => {
+		const { wrapper } = mountProductCard(server);
+
+		expect(wrapper.element).toMatchSnapshot();
+	});
+
+	it('Should emit the event addToCart with the product object when button gets clicked.', async () => {
+		const { wrapper, product } = mountProductCard(server);
+
+		await wrapper.find('button').trigger('click');
+
+		expect(wrapper.emitted().addToCart).toBeTruthy();
+		expect(wrapper.emitted().addToCart.length).toBe(1);
+		expect(wrapper.emitted().addToCart[0]).toEqual([{ product }]);
+	});
+});
