7d5c5cecad3dc89be1b5240df19722ce386ff845
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2023-02-22 00:17:19 -0300

    test(01-vanilla-js): fixed broken tests

--- 01-vanilla-js/src/lib/calculator.spec.js
+++ 01-vanilla-js/src/lib/calculator.spec.js
@@ -7,7 +7,6 @@ it('Should sum 2 and 2 and the result must be 4.', () => {
 
 it('Should sum "2" and 2 even if one of them is a string and the result must keep being 4.', () => {
 	expect(sum('2', 2)).toBe(4);
-	expect(false).toBe(true);
 });
 
 it('Should throw an error if one of the operators cannot be converted into a number.', () => {
