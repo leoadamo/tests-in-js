4805a8a1254abacacb26e40dd610148556ad6a73
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2023-02-22 11:37:29 -0300

    feat(02-vue-nuxt): adding the project components
    Adding the components, layouts and pages that will be used during the tests

---
+++ 02-vue-nuxt/components/CartItem.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="flex justify-between mt-6">
+		<div class="flex">
+			<img
+				class="h-20 w-20 object-cover rounded"
+				src="https://images.unsplash.com/photo-1524592094714-0f0654e20314?ixlib=rb-1.2.1&auto=format&fit=crop&w=689&q=80"
+				alt="Wrist watch classic"
+			/>
+			<div class="mx-3">
+				<h3 class="text-sm text-gray-600">Wrist watch classic</h3>
+				<button data-testid="remove-button">remover</button>
+				<div class="flex items-center mt-2">
+					<button
+						data-testid="-"
+						class="text-gray-500 focus:outline-none focus:text-gray-600"
+					>
+						<svg
+							class="h-5 w-5"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+						</svg>
+					</button>
+					<span data-testid="quantity" class="text-gray-700 mx-2"> 1 </span>
+					<button
+						data-testid="+"
+						class="text-gray-500 focus:outline-none focus:text-gray-600"
+					>
+						<svg
+							class="h-5 w-5"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+							></path>
+						</svg>
+					</button>
+				</div>
+			</div>
+		</div>
+		<span class="text-gray-600">$120.00</span>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CartItem',
+};
+</script>
--- 02-vue-nuxt/pages/index.vue
+++ 02-vue-nuxt/pages/index.vue
@@ -1,9 +1,33 @@
 <template>
-	<Tutorial />
+	<main class="my-8">
+		<search-bar />
+		<div class="container mx-auto px-6">
+			<h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
+			<span
+				data-testid="total-quantity-label"
+				class="mt-3 text-sm text-gray-500"
+			>
+				12 items found
+			</span>
+			<div
+				class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
+			>
+				<product-card
+					v-for="(product, index) in 12"
+					:key="index"
+					data-testid="product-card"
+				/>
+			</div>
+		</div>
+		<!-- <h3 v-else class="text-center text-2xl">{{ errorMessage }}</h3> -->
+	</main>
 </template>
 
 <script>
+import ProductCard from '@/components/ProductCard';
+import SearchBar from '@/components/SearchBar';
+
 export default {
-	name: 'IndexPage',
+	components: { ProductCard, SearchBar },
 };
 </script>
