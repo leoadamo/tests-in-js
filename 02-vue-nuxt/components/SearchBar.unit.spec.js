4886d3b657a5bdda04a0e330b71b2ca74f1284da
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2023-02-22 22:09:31 -0300

    test(02-vue-nuxt): adding searchBar tests and doSearch event

---
+++ 02-vue-nuxt/components/SearchBar.unit.spec.js
@@ -0,0 +1,41 @@
+// DEPENDENCIES
+import { mount } from '@vue/test-utils';
+
+// COMPONENTS
+import SearchBar from '@/components/SearchBar';
+
+describe('SearchBar - Unit', () => {
+	it('Should mount the component.', () => {
+		const wrapper = mount(SearchBar);
+
+		expect(wrapper.vm).toBeTruthy();
+	});
+
+	it('Should emit a "doSearch" event, containing a search term as payload when the form is submitted.', async () => {
+		const wrapper = mount(SearchBar);
+
+		const searchTerm = 'Este é o termo de busca';
+
+		await wrapper.find('input[type="search"]').setValue(searchTerm);
+		await wrapper.find('form').trigger('submit');
+
+		expect(wrapper.emitted().doSearch).toBeTruthy();
+		expect(wrapper.emitted().doSearch.length).toBe(1);
+		expect(wrapper.emitted().doSearch[0]).toEqual([{ searchTerm }]);
+	});
+
+	it('Should emit a "doSearch" event, containing an empty search term as payload when the input field is cleared.', async () => {
+		const wrapper = mount(SearchBar);
+
+		const input = wrapper.find('input[type="search"]');
+
+		const searchTerm = 'Este é o termo de busca';
+
+		await input.setValue(searchTerm);
+		await input.setValue('');
+
+		expect(wrapper.emitted().doSearch).toBeTruthy();
+		expect(wrapper.emitted().doSearch.length).toBe(1);
+		expect(wrapper.emitted().doSearch[0]).toEqual([{ searchTerm: '' }]);
+	});
+});
--- 02-vue-nuxt/components/SearchBar.vue
+++ 02-vue-nuxt/components/SearchBar.vue
@@ -1,5 +1,9 @@
 <template>
-	<form data-testid="search-form" class="relative mt-6 max-w-lg mx-auto">
+	<form
+		data-testid="search-form"
+		class="relative mt-6 max-w-lg mx-auto"
+		@submit.prevent="doSearch"
+	>
 		<span class="absolute inset-y-0 left-0 pl-3 flex items-center">
 			<svg class="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
 				<path
@@ -12,6 +16,7 @@
 			</svg>
 		</span>
 		<input
+			v-model="searchTerm"
 			class="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
 			type="search"
 			placeholder="Search"
@@ -22,5 +27,25 @@
 <script>
 export default {
 	name: 'SearchBar',
+
+	data() {
+		return {
+			searchTerm: null,
+		};
+	},
+
+	watch: {
+		searchTerm(newValue) {
+			if (!newValue) {
+				this.$emit('doSearch', { searchTerm: newValue });
+			}
+		},
+	},
+
+	methods: {
+		doSearch() {
+			this.$emit('doSearch', { searchTerm: this.searchTerm });
+		},
+	},
 };
 </script>
