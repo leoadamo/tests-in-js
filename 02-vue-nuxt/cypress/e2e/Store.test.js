0454f9c6677d72c4a669b331bd51baec1c1821b1
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2023-03-05 13:13:07 -0300

    test(02-vue-nuxt): adding ProductsList e2e tests

--- 02-vue-nuxt/cypress/e2e/Store.test.js
+++ 02-vue-nuxt/cypress/e2e/Store.test.js
@@ -13,14 +13,14 @@ context("The Lunch", () => {
   });
 
   it("Should display the home page.", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("/");
     cy.get("body").contains("Brand");
     cy.get("body").contains("The lunch");
   });
 
-  context("The Lunch > Searching for a product", () => {
+  context("The Lunch > Searching for a food", () => {
     it("Should type in the search field.", () => {
-      cy.visit("http://localhost:3000");
+      cy.visit("/");
       cy.get("input[type='search']")
         .type("Some text here")
         .should("have.value", "Some text here");
@@ -32,7 +32,7 @@ context("The Lunch", () => {
         title: "Hamburger Cia 09",
       });
 
-      cy.visit("http://localhost:3000");
+      cy.visit("/");
       cy.get("input[type='search']").type("Hamburger Cia 09");
       cy.get('[data-testid="search-form"]').submit();
       cy.get('[data-testid="product-card"]').should("have.length", 1);
@@ -41,11 +41,35 @@ context("The Lunch", () => {
     it("Should type in the search field and shouldn't have any product returned.", () => {
       Server.createList("product", 10);
 
-      cy.visit("http://localhost:3000");
+      cy.visit("/");
       cy.get("input[type='search']").type("Hamburger Cia 09");
       cy.get('[data-testid="search-form"]').submit();
       cy.get('[data-testid="product-card"]').should("have.length", 0);
       cy.get("body").contains("0 items found");
+    });
+  });
+
+  context("The Lunch > Rendering the list of foods", () => {
+    it('Should display "0 items found when no items is returned."', () => {
+      cy.visit("/");
+      cy.get('[data-testid="product-card"]').should("have.length", 0);
+      cy.get("body").contains("0 items found");
+    });
+
+    it('Should display "1 item found" when 1 item is returned.', () => {
+      Server.create("product");
+
+      cy.visit("/");
+      cy.get('[data-testid="product-card"]').should("have.length", 1);
+      cy.get("body").contains("1 item found");
+    });
+
+    it('Should display "10 items found when 10 items are returned."', () => {
+      Server.createList("product", 10);
+
+      cy.visit("/");
+      cy.get('[data-testid="product-card"]').should("have.length", 10);
+      cy.get("body").contains("10 items found");
     });
   });
 });
