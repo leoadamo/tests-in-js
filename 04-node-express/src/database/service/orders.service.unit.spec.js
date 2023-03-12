9d9214f0df36a809dd9d033527d77ede1c74429d
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2023-03-12 16:11:45 -0300

    test(04-node-express): adding orders service unit tests

---
+++ 04-node-express/src/database/service/orders.service.unit.spec.js
@@ -0,0 +1,89 @@
+// SERVICES
+import { listOrders, saveOrder } from './orders.service';
+
+// MODELS
+import { Order } from '@/database/models/order.model';
+
+// BUILDERS
+import { buildError, buildOrder, buildOrders, buildUser } from '~/builders';
+import { StatusCodes } from 'http-status-codes';
+
+import { logger } from '@/utils/logger';
+
+jest.mock('@/database/models/order.model');
+jest.mock('@/utils/logger');
+
+// JSON.parse = jest.fn();
+
+describe('Service > Orders', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Should return a list of orders.', async () => {
+    const user = buildUser();
+
+    const where = {
+      userid: user.id,
+    };
+
+    const orders = buildOrders().map(order => {
+      const orderCopy = order;
+
+      orderCopy.products = JSON.stringify(orderCopy.products);
+
+      return orderCopy;
+    });
+
+    jest.spyOn(Order, 'findAll').mockResolvedValueOnce(orders);
+
+    const returnedOrders = await listOrders(user.id);
+
+    expect(returnedOrders).toEqual(orders);
+    expect(Order.findAll).toHaveBeenCalledTimes(1);
+    expect(Order.findAll).toHaveBeenCalledWith({ where });
+    // expect(JSON.parse).toHaveBeenCalledTimes(orders.length);
+  });
+
+  it('Should reject with an error when Order.findAll() fails.', () => {
+    const user = buildUser();
+
+    const error = buildError(
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      `Failed to retrieve orders for user: ${user.id}`,
+    );
+
+    jest.spyOn(Order, 'findAll').mockRejectedValueOnce(error);
+
+    expect(listOrders(user.id)).rejects.toEqual(error);
+  });
+
+  it('Should save and return an order.', () => {
+    const user = buildUser();
+
+    const data = {
+      useris: user.id,
+      products: buildOrder(),
+    };
+
+    const order = {
+      ...data,
+      id: 1,
+    };
+
+    jest.spyOn(Order, 'create').mockResolvedValueOnce(order);
+
+    expect(saveOrder(data)).resolves.toEqual(order);
+    expect(logger.info).toHaveBeenCalledTimes(1);
+    expect(logger.info).toHaveBeenCalledWith(`New order saved`, { data });
+  });
+
+  it('Should reject with an error when saveOrder() is called without any data.', () => {
+    const error = buildError(
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      'Failed to save order.',
+    );
+
+    expect(saveOrder()).rejects.toEqual(error);
+  });
+});
