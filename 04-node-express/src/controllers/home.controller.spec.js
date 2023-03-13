fdf5a264eacb92a7652bd86bdd40ba81ae940215
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2023-03-12 22:55:08 -0300

    test(04-node-express): adding missing home.controller unit test

--- 04-node-express/src/controllers/home.controller.spec.js
+++ 04-node-express/src/controllers/home.controller.spec.js
@@ -1,5 +1,22 @@
-describe('HomeController - Unit', () => {
-  it('Should be an example.', () => {
-    expect(1).toBe(1);
+// DEPENDENCIES
+import { StatusCodes } from 'http-status-codes';
+
+// CONTROLLERS
+import { index } from './home.controller';
+
+describe('Controllers > Home - Unit', () => {
+  const res = {
+    status: jest.fn(() => res),
+    json: jest.fn(() => res),
+  };
+
+  it('Should return status code 200 with a message when accessing the endpoint.', async () => {
+    await index(null, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ welcome: 'Welcome Stranger!' });
   });
 });
